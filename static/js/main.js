cd97e464e8b44520b63af297bcbdd0802f800fb3
mmhasanovee <ME.OVEE@GMAIL.COM>
2019-09-16 07:41:03 +0600

    uuuu

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,12 @@
 (function ($) {
     "use strict";
 
-    
+
     /*==================================================================
     [ Validate ]*/
     var name = $('.validate-input input[name="name"]');
     var email = $('.validate-input input[name="email"]');
     var message = $('.validate-input textarea[name="message"]');
-
 
     $('.validate-form').on('submit',function(){
         var check = true;
@@ -17,19 +16,19 @@
             showValidate(name);
             check=false;
         }
-
-     
-
-
         if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
             showValidate(email);
             check=false;
         }
-
         if($(message).val().trim() == ''){
             showValidate(message);
             check=false;
         }
+        if(check == true){
+            $("#loading").show();
+            $("#content").hide();
+        }
+
 
         return check;
     });
@@ -43,16 +42,14 @@
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).addClass('alert-validate');
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
-
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
+
+
 
 })(jQuery);
